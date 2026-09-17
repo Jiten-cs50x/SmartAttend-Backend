be9d282ded5208b7f75c262c323f0760a9f7f998
Jiten <jitenkalal321@gmail.com>
2026-09-18 00:56:23 +0530

    made changes to timetablemanagementcontroller and timetablecontroller, also added studentparser.js

--- src/routes/timetableRoutes.js
+++ src/routes/timetableRoutes.js
@@ -4,10 +4,12 @@ import {
   getTimetable,
   createTimetable,
 } from "../controllers/timetableController.js";
+import { authenticate } from "../middleware/authMiddleware.js";
+import { authorize } from "../middleware/roleMiddleware.js";
 
 const router = express.Router();
 
 router.get("/", getTimetable);
-router.post("/", createTimetable);
+router.post("/", authenticate, authorize("ADMIN", "HOD"), createTimetable);
 
 export default router;
